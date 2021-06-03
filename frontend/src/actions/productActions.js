40c756689d1965f31a5e3d91c68ed97d282753b7
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-06-03 03:40:05 -0700

    Added Reducer, Action, and Constants to Redux. Called API from action which will be called by component page

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import {
+    PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL
+} from '../constants/productConstants'
+//dispatch outputs the switch actions
+//thunk allows us to call async and add func to a func
+const listProducts = () => async (dispatch)=> {
+    try{
+        dispatch({type:PRODUCT_LIST_REQUEST})
+        const {data} = await axios.get('/api/products/')
+        dispatch({
+            type:PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    }catch(error){
+        dispatch({
+            type:PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message
+            ? error.response.data.message : error.message,
+        })
+    }
+}
